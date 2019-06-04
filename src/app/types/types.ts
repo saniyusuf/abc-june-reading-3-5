a3a474c0db0fc692cddbf4f56e56b8e25d370d89
Sani Yusuf <saniyusuf@gmail.com>
2019-06-04 13:41:30 -0400

    202 commit

---
+++ src/app/types/types.ts
@@ -0,0 +1,12 @@
+export interface Video {
+  title: string;
+  author: string;
+  id: string;
+  viewDetails: ViewDetail[];
+}
+
+interface ViewDetail {
+  age: number;
+  region: string;
+  date: string;
+}
