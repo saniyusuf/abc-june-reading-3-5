03aaffdb0b7b54d5b4bace17d0e0f3989612cf2b
Sani Yusuf <saniyusuf@gmail.com>
2019-06-05 10:05:05 -0400

    300 commit

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { VideoDashboardComponent } from './video-dashbaord/video-dashboard.component';
-import { VideoListComponent } from './video-list/video-list.component';
-import { VideoPlayerComponent } from './video-player/video-player.component';
-import { StatFiltersComponent } from './stat-filters/stat-filters.component';
-import {RouterModule, Routes} from '@angular/router';
-import { VideoComponent } from './video/video.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { VideoDashboardComponent } from "./video-dashbaord/video-dashboard.component";
+import { VideoListComponent } from "./video-list/video-list.component";
+import { VideoPlayerComponent } from "./video-player/video-player.component";
+import { StatFiltersComponent } from "./stat-filters/stat-filters.component";
+import { RouterModule, Routes } from "@angular/router";
+import { VideoComponent } from "./video/video.component";
+import { HttpClientModule } from "@angular/common/http";
 
 const childRoutes: Routes = [
   {
@@ -16,10 +17,13 @@ const childRoutes: Routes = [
 ];
 
 @NgModule({
-  declarations: [VideoDashboardComponent, VideoListComponent, VideoPlayerComponent, StatFiltersComponent, VideoComponent],
-  imports: [
-    CommonModule,
-    RouterModule.forChild(childRoutes)
-  ]
+  declarations: [
+    VideoDashboardComponent,
+    VideoListComponent,
+    VideoPlayerComponent,
+    StatFiltersComponent,
+    VideoComponent
+  ],
+  imports: [CommonModule, RouterModule.forChild(childRoutes), HttpClientModule]
 })
-export class DashboardModule { }
+export class DashboardModule {}
--- src/app/dashboard/video-dashbaord/video-dashboard.component.ts
+++ src/app/dashboard/video-dashbaord/video-dashboard.component.ts
@@ -1,209 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { Video } from '../../types/types';
+import { Component, OnInit } from "@angular/core";
+import { Video } from "../../types/types";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-video-dashboard',
-  templateUrl: './video-dashboard.component.html',
-  styleUrls: ['./video-dashboard.component.css']
+  selector: "app-video-dashboard",
+  templateUrl: "./video-dashboard.component.html",
+  styleUrls: ["./video-dashboard.component.css"]
 })
 export class VideoDashboardComponent implements OnInit {
-  videoListData: Video[] = videoData;
+  videoListData: Video[] = [];
   currentlySelectedVideo: Video;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(http: HttpClient) {
+    http
+      .get<Video[]>("https://api.angularbootcamp.com/videos")
+      .subscribe(videoList => (this.videoListData = videoList));
   }
+
+  ngOnInit() {}
 
   setSelectedVideo(video: Video) {
     this.currentlySelectedVideo = video;
   }
 }
-
-const videoData = [
-  {
-    "title": "Angular Observable Data Flow",
-    "author": "Kyle Cordes",
-    "id": "JPuqluYYa-o",
-    "viewDetails": [
-      {
-        "age": 17,
-        "region": "North America",
-        "date": "2016-03-23"
-      },
-      {
-        "age": 27,
-        "region": "North America",
-        "date": "2016-03-23"
-      },
-      {
-        "age": 37,
-        "region": "North America",
-        "date": "2016-03-23"
-      },
-      {
-        "age": 47,
-        "region": "Europe",
-        "date": "2016-03-24"
-      },
-      {
-        "age": 37,
-        "region": "North America",
-        "date": "2016-03-24"
-      },
-      {
-        "age": 17,
-        "region": "North America",
-        "date": "2016-03-25"
-      }
-    ]
-  },
-  {
-    "title": "Angular Performance Checklist",
-    "author": "Paul Spears",
-    "id": "cxqRijt9LbQ",
-    "viewDetails": [
-      {
-        "age": 36,
-        "region": "North America",
-        "date": "2016-06-23"
-      },
-      {
-        "age": 30,
-        "region": "North America",
-        "date": "2016-06-23"
-      },
-      {
-        "age": 54,
-        "region": "North America",
-        "date": "2016-07-23"
-      },
-      {
-        "age": 43,
-        "region": "Europe",
-        "date": "2016-0-24"
-      },
-      {
-        "age": 32,
-        "region": "North America",
-        "date": "2016-08-24"
-      },
-      {
-        "age": 32,
-        "region": "North America",
-        "date": "2016-08-25"
-      }
-    ]
-  },
-  {
-    "title": "Live App Updates Without The App Store",
-    "author": "Sani Yusuf",
-    "id": "s10wrXA-a7Y",
-    "viewDetails": [
-      {
-        "age": 17,
-        "region": "North America",
-        "date": "2016-03-23"
-      },
-      {
-        "age": 27,
-        "region": "North America",
-        "date": "2016-03-23"
-      },
-      {
-        "age": 37,
-        "region": "North America",
-        "date": "2016-03-23"
-      },
-      {
-        "age": 47,
-        "region": "Europe",
-        "date": "2016-03-24"
-      },
-      {
-        "age": 37,
-        "region": "North America",
-        "date": "2016-03-24"
-      },
-      {
-        "age": 17,
-        "region": "North America",
-        "date": "2016-03-25"
-      }
-    ]
-  },
-  {
-    "title": "Angular Reactive Forms",
-    "author": "Jack Balbes",
-    "id": "A_Rq6ZsoXpI",
-    "viewDetails": [
-      {
-        "age": 36,
-        "region": "North America",
-        "date": "2016-06-23"
-      },
-      {
-        "age": 30,
-        "region": "North America",
-        "date": "2016-06-23"
-      },
-      {
-        "age": 54,
-        "region": "North America",
-        "date": "2016-07-23"
-      },
-      {
-        "age": 43,
-        "region": "Europe",
-        "date": "2016-0-24"
-      },
-      {
-        "age": 32,
-        "region": "North America",
-        "date": "2016-08-24"
-      },
-      {
-        "age": 32,
-        "region": "North America",
-        "date": "2016-08-25"
-      }
-    ]
-  },
-  {
-    "title": "Imperative to Reactive with Angular and RxJS",
-    "author": "John Baur",
-    "id": "VJOPsjlbhdg",
-    "viewDetails": [
-      {
-        "age": 17,
-        "region": "North America",
-        "date": "2016-03-23"
-      },
-      {
-        "age": 27,
-        "region": "North America",
-        "date": "2016-03-23"
-      },
-      {
-        "age": 37,
-        "region": "North America",
-        "date": "2016-03-23"
-      },
-      {
-        "age": 47,
-        "region": "Europe",
-        "date": "2016-03-24"
-      },
-      {
-        "age": 37,
-        "region": "North America",
-        "date": "2016-03-24"
-      },
-      {
-        "age": 17,
-        "region": "North America",
-        "date": "2016-03-25"
-      }
-    ]
-  }
-];
